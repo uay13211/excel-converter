94bf54d6ab21b5b69bc296c0944a4a03de8c9ac5
uay13211 <54673489+uay13211@users.noreply.github.com>
2019-10-05 19:40:17 +0800

    Buttons instead of menubar create individual folder for different excel file replace the space of the file name with _

--- GUI.py
+++ GUI.py
@@ -14,25 +14,26 @@ def convert():
 
         try:
             excelfile_path = os.path.join(r'', xls_file)
+            wb = o.Workbooks.Open(excelfile_path)
 
         except:
             messagebox.showerror(title='Error', message='Please select a excel file')
 
+        filename_noextension = os.path.splitext(os.path.basename(xls_file))[0].replace(' ', '_')
+        save_path = os.path.join(window.savedir, filename_noextension)
+        if not os.path.exists(save_path):
+            os.mkdir(save_path)
+
         for sh in wb.Sheets:
-
-            wb = o.Workbooks.Open(excelfile_path)
-            filename_noextension = os.path.splitext(os.path.basename(xls_file))[0]
-            save_path = os.path.join(window.savedir, filename_noextension)
-
-            if not os.path.exists(save_path):
-                os.mkdir(save_path)
+            sheet_name_repalce = sh.Name.replace(' ', '_')
             try:
-                pdf_path = os.path.join(r"", save_path, sh.Name + ".pdf")
+                pdf_path = os.path.join(r"", save_path, sheet_name_repalce + ".pdf")
             except:
                 messagebox.showerror(title='Error', message='Please provide an valid path')
 
-            wb.Worksheets(sh.Name).Select()
-            wb.ActiveSheet.ExportAsFixedFormat(0, pdf_path)
+            if not os.path.exists(pdf_path):
+                wb.Worksheets(sh.Name).Select()
+                wb.ActiveSheet.ExportAsFixedFormat(0, pdf_path)
 
     o.Quit()
 
@@ -42,25 +43,22 @@ def openfile():
                                                    filetypes=(('xls files', '*.xls'), ('xlsx files', '*.xlsx')))
     print(window.filenames)
 
+
 def savefile():
     window.savedir = filedialog.askdirectory()
+
 
 window = tk.Tk()
 
 window.title('Excel converter')
-window.geometry('500x300')
-menubar = tk.Menu(window)
-filemenu = tk.Menu(menubar, tearoff=0)
+window.geometry('300x300')
 
-menubar.add_cascade(label='File', menu=filemenu)
+open_button = tk.Button(window, text='Open File', command=openfile)
+save_as_button = tk.Button(window, text='Save in', command=savefile)
+convert_button = tk.Button(window, text='Convert', command=convert)
 
-filemenu.add_command(label='Open', command=openfile)
-filemenu.add_command(label='Save as', command=savefile)
-
-window.config(menu=menubar)
-
-
-button_convert = tk.Button(window, text='convert', command=convert)
-button_convert.pack()
+open_button.place(height=50, width=200, relx=0.17, rely=0.15)
+save_as_button.place(height=50, width=200, relx=0.17, rely=0.4)
+convert_button.place(height=50, width=200, relx=0.17, rely=0.65)
 
 window.mainloop()
